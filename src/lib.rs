79e0193c552adbb4ef58e4e0c9d2ea90a2da9b50
Benjamin Saunders <ben.e.saunders@gmail.com>
2021-02-07 13:07:04 -0800

    Reexport alignment helper

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ mod ring_alloc;
 
 pub use async_queue::AsyncQueue;
 pub use memory::{
-    alloc_bind, DedicatedBuffer, DedicatedImage, DedicatedMapping, MemoryResource, Staged,
+    align, alloc_bind, DedicatedBuffer, DedicatedImage, DedicatedMapping, MemoryResource, Staged,
 };
 pub use region::{BufferRegion, BufferRegionAlloc, ImageRegion};
 pub use staging::StagingBuffer;
